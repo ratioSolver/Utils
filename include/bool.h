f4a5c8a41dace023f531923fe1c2b9676353a220
riccardodebenedictis <riccardo.debenedictis@istc.cnr.it>
2023-02-18 12:54:26 +0100

    minor cleanings..

---
+++ include/bool.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace utils
+{
+  using lbool = unsigned short int;
+  constexpr lbool False = 0;
+  constexpr lbool True = 1;
+  constexpr lbool Undefined = 2;
+} // namespace utils
