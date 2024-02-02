e1db2c5bbf96fb42df0f816d0cfb81b9017be427
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-02-02 11:04:58 +0100

    Refactor tableau class to support pivot operation

--- include/tableau.hpp
+++ include/tableau.hpp
@@ -6,6 +6,18 @@
 
 namespace utils
 {
+  /**
+   * @brief Manage a tableau of linear expressions.
+   *
+   * The tableau is a map of linear expressions, where each expression is associated with a variable.
+   * Formally, the tableau represents the following system of linear equations:
+   *
+   * `x = Ay + b`
+   *
+   * where `x` is the vector of variables representing the base of the tableau, `A` is the matrix of coefficients, `y` is the vector of non-basic variables, and `b` is the vector of known terms.
+   * Since the tableau is usually very sparse, we use a map to represent it, having the basic variables as keys and the linear expressions as values.
+   * We also keep track, for each non-basic variable, of the linear expressions that contain it, indexed by their corresponding basic variable, in order to speed up the pivot operation.
+   */
   class tableau final
   {
   public:
@@ -23,12 +35,16 @@ namespace utils
     void add_row(const VARIABLE_TYPE x_i, lin &&expr);
 
     /**
-     * @brief Pivot the tableau using the variable `x_i` and the variable `x_j`.
+     * @brief Pivot the tableau using the variable `x_i` and the variable `y_j`.
+     *
+     * The pivot operation is used to transform the tableau in order to solve the linear system of equations.
+     * The `x = Ay + b` system is transformed into `x' = A'y' + b'`, where `x'` is the new base of the tableau, `A'` is the new matrix of coefficients, `y'` is the new vector of non-basic variables, and `b'` is the new vector of known terms.
+     * The `x_i = ...` row is replaced by the `y_j = ...` row, updating all the rows that contain `y_j` in their linear expression.
      *
      * @param x_i the leaving variable.
-     * @param x_j the entering variable.
+     * @param y_j the entering variable.
      */
-    void pivot(const VARIABLE_TYPE x_i, const VARIABLE_TYPE x_j);
+    void pivot(const VARIABLE_TYPE x_i, const VARIABLE_TYPE y_j) noexcept;
 
     friend std::string to_string(const tableau &t) noexcept;
 
--- src/tableau.cpp
+++ src/tableau.cpp
@@ -11,46 +11,53 @@ namespace utils
 
     void tableau::add_row(const VARIABLE_TYPE x_i, lin &&expr)
     {
+        assert(table.find(x_i) == table.cend());
         for (const auto &x : expr.vars)
             watches[x.first].insert(x_i);
         table.emplace(x_i, std::move(expr));
     }
 
-    void tableau::pivot(const VARIABLE_TYPE x_i, const VARIABLE_TYPE x_j)
-    {
+    void tableau::pivot(const VARIABLE_TYPE x_i, const VARIABLE_TYPE y_j) noexcept
+    { // `x_i` is the leaving variable, `y_j` is the entering variable
+        assert(table.find(x_i) != table.cend() && "x_i is not a basic variable");
+        assert(table.find(y_j) == table.cend() && "y_j is not a non-basic variable");
+
+        // we remove the leaving variable `x_i` from the watches
         for (const auto &x : table[x_i].vars)
             watches[x.first].erase(x_i);
 
-        // we rewrite the linear expression of the leaving variable x_i
+        // we rewrite `x_i = ...` as `y_j = ...`
         lin l = std::move(table[x_i]);
-        rational c = l.vars.at(x_j);
-        l.vars.erase(x_j);
+        rational c = l.vars.at(y_j);
+        l.vars.erase(y_j);
         l /= -c;
         l.vars.emplace(x_i, rational::one / c);
         table.erase(x_i);
 
-        for (auto &x : watches[x_j])
+        // we update the rows that contain `y_j`
+        for (auto &x : watches[y_j])
         {
-            rational &c = table[x].vars.at(x_j);
+            rational &c = table[x].vars.at(y_j);
             for (const auto &term : l.vars)
                 if (const auto trm_it = table[x].vars.find(term.first); trm_it == table[x].vars.cend())
-                {
-                    table[x].vars.emplace(term.first, c * term.second);
-                    watches[term.first].insert(x);
+                {                                                       // `term.first` is not in the linear expression of `x`, so we add it
+                    table[x].vars.emplace(term.first, c * term.second); // we add `c * term.second` to the linear expression of `x`
+                    watches[term.first].insert(x);                      // we add `x` to the watches of `term.first`
                 }
                 else
                 {
                     trm_it->second += c * term.second;
                     if (trm_it->second == 0)
-                    {
-                        table[x].vars.erase(trm_it);
-                        watches[term.first].erase(x);
+                    {                                 // if the coefficient of `term.first` is zero, we remove the term from the linear expression
+                        table[x].vars.erase(trm_it);  // we remove `term.first` from the linear expression of `x`
+                        watches[term.first].erase(x); // we remove `x` from the watches of `term.first`
                     }
                 }
             table[x].known_term += c * l.known_term;
         }
 
-        add_row(x_j, std::move(l));
+        // we add the new row `y_j = ...`
+        add_row(y_j, std::move(l));
     }
 
     std::string to_string(const tableau &t) noexcept
