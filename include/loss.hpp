b168b380f3ba7275f70267e428590623789fd989
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-10-19 12:40:42 +0200

    Refactor loss function to calculate Mean Squared Error (MSE)
    The `loss` function in `loss.hpp` has been refactored to calculate the Mean Squared Error (MSE) between two arrays. This measure is commonly used in regression analysis to assess the accuracy of a model. The function now takes pointers to the arrays of true values and predicted values, as well as the number of elements in the arrays. It calculates the sum of squared differences between the elements and returns the mean squared error.

--- include/loss.hpp
+++ include/loss.hpp
@@ -1,28 +1,26 @@
 #pragma once
 
-#include <array>
+#include <cstddef>
 
 namespace utils
 {
   /**
-   * @brief Computes the loss between the true values and the predicted values.
+   * @brief Computes the Mean Squared Error (MSE) between two arrays.
    *
-   * This function calculates the sum of squared differences between the elements
-   * of the true values array (`y_true`) and the predicted values array (`y_pred`).
+   * This function calculates the MSE, which is a measure of the average squared difference
+   * between the predicted values and the actual values. It is commonly used in regression
+   * analysis to assess the accuracy of a model.
    *
-   * @tparam Fn Number of features.
-   * @tparam Sn Number of samples.
-   * @param y_true A 2D array containing the true values.
-   * @param y_pred A 2D array containing the predicted values.
-   * @return The computed loss as a double.
+   * @param y_true Pointer to the array of true values.
+   * @param y_pred Pointer to the array of predicted values.
+   * @param n The number of elements in the arrays.
+   * @return The mean squared error between the true and predicted values.
    */
-  template <std::size_t Sn, std::size_t Fn>
-  double loss(const std::array<std::array<double, Fn>, Sn> &y_true, const std::array<std::array<double, Fn>, Sn> &y_pred)
+  double mse(const double *y_true, const double *y_pred, const std::size_t n)
   {
-    double loss = 0.0;
-    for (std::size_t i = 0; i < Sn; ++i)
-      for (std::size_t j = 0; j < Fn; ++j)
-        loss += (y_true[i][j] - y_pred[i][j]) * (y_true[i][j] - y_pred[i][j]);
-    return loss;
+    double sum = 0;
+    for (std::size_t i = 0; i < n; i++)
+      sum += (y_true[i] - y_pred[i]) * (y_true[i] - y_pred[i]);
+    return sum / n;
   }
 } // namespace utils
