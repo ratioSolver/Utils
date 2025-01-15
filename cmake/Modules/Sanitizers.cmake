8bb4edda15fa653dcd0474effdb41972954c95f4
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-01-15 04:39:31 +0100

    Refactor CMake configuration to centralize sanitizer setup and streamline test options

---
+++ cmake/Modules/Sanitizers.cmake
@@ -0,0 +1,17 @@
+# Sanitizers.cmake
+# A CMake module to simplify sanitizer setup.
+
+function(setup_sanitizers target)
+    if(MSVC)
+        target_compile_options(${target} PRIVATE /W4)
+    else()
+        target_compile_options(${target} PRIVATE -Wall -Wextra -Wpedantic -fsanitize=undefined)
+        target_link_options(${target} PRIVATE -fsanitize=undefined)
+        if (ENABLE_COVERAGE)
+            if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+                target_compile_options(${target} PRIVATE --coverage)
+                target_link_libraries(${target} PUBLIC gcov)
+            endif()
+        endif()
+    endif()
+endfunction()
