87a0359608e9d4d0a0a40bebfe1fba3b6caec763
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-01-17 15:07:20 +0100

    Refactor sanitizer setup in CMake to improve error handling and enable coverage options

--- cmake/Modules/Sanitizers.cmake
+++ cmake/Modules/Sanitizers.cmake
@@ -2,16 +2,46 @@
 # A CMake module to simplify sanitizer setup.
 
 function(setup_sanitizers target)
+    if (MSVC AND (ENABLE_MSAN OR ENABLE_USAN))
+        message(FATAL_ERROR "Only address sanitizer is supported on Windows")
+    endif()
+
+    if (ENABLE_ASAN AND ENABLE_MSAN)
+        message(FATAL_ERROR "Address and memory sanitizers cannot be enabled at the same time")
+    endif()
+
+    set(SANITIZERS "")
+
+    if (ENABLE_ASAN)
+        list(APPEND SANITIZERS "address")
+    endif()
+
+    if (ENABLE_MSAN)
+        list(APPEND SANITIZERS "memory")
+    endif()
+
+    if (ENABLE_USAN)
+        list(APPEND SANITIZERS "undefined")
+    endif()
+
+    list(JOIN SANITIZERS "," LIST_OF_SANITIZERS)
+
     if(MSVC)
         target_compile_options(${target} PRIVATE /W4)
     else()
-        target_compile_options(${target} PRIVATE -Wall -Wextra -Wpedantic -fsanitize=undefined,address)
-        target_link_options(${target} PRIVATE -fsanitize=undefined,address)
-        if (ENABLE_COVERAGE)
+        target_compile_options(${target} PRIVATE -Wall -Wextra -Wpedantic)
+        if (ENABLE_COV)
             if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-                target_compile_options(${target} PRIVATE --coverage)
-                target_link_libraries(${target} PUBLIC gcov)
+                message(STATUS "Code coverage enabled for ${target}")
+                target_compile_options(${target} INTERFACE --coverage)
+                target_link_libraries(${target} INTERFACE --coverage)
             endif()
         endif()
+    endif()
+
+    if (NOT "${LIST_OF_SANITIZERS}" STREQUAL "")
+        message(STATUS "Sanitizers enabled for ${target}: ${LIST_OF_SANITIZERS}")
+        target_compile_options(${target} INTERFACE -fsanitize=${LIST_OF_SANITIZERS})
+        target_link_options(${target} INTERFACE -fsanitize=${LIST_OF_SANITIZERS})
     endif()
 endfunction()
